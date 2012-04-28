38cd9134f2e8a6f4c64da6d1618a48895369084a
naoaki011 <naoaki@orion.ocn.ne.jp>
2012-04-28 18:57:10 +0900

    closes #1,#2 add L10N. bump up version.

---
+++ plugins/EntryNumberByDay/lib/EntryNumberByDay/L10N/en_us.pm
@@ -0,0 +1,10 @@
+package EntryNumberByDay::L10N::en_us;
+
+use strict;
+use base 'EntryNumberByDay::L10N';
+use vars qw( %Lexicon );
+
+%Lexicon = (
+);
+
+1;
---
+++ plugins/EntryNumberByDay/lib/EntryNumberByDay/L10N/ja.pm
@@ -0,0 +1,14 @@
+package EntryNumberByDay::L10N::ja;
+
+use strict;
+use base 'EntryNumberByDay::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+    'Output number of published order in same day.'
+      => '同日に投稿された記事の順番を出力します。',
+);
+
+1;
